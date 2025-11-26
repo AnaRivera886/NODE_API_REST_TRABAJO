0889d17a58f78e5c2b02ed1381c27aa01ff12a4b
Ana Rivera <asrivera886@gmail.com>
2025-11-26 10:46:35 -0500

    feat: add basic tasks CRUD with prisma/2

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -5,5 +5,8 @@ import { getAllTasks } from '../controllers/tasks.controller.js';
 const router = express.Router();
 
 router.get('/tasks', getAllTasks);
+router.post('/tasks', CreateTasks);
+router.put('/tasks/:id', updateTasks);
+router.delete('/tasks/:id', deleteTasks);
 
 export default router;
